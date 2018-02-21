b8e365aa2d381319b933b26c0a91aa0729c0219b
roman <roma.sluka.97@mail.ru>
2018-02-22 02:41:07 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,31 +1,45 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+    constructor() {
+        this.mas = [];
+        this.compareFunction = function compareNumeric(a, b) {
+            return a - b;
+        }
+    }
 
-  add(element) {
-    // your implementation
-  }
+    add(element) {
+        this.mas.push(element);
+    }
 
-  at(index) {
-    // your implementation
-  }
+    at(index) {
+        return this.mas[index];
+    }
 
-  get length() {
-    // your implementation
-  }
+    get length() {
+        return this.mas.length;
+    }
 
-  toArray() {
-    // your implementation
-  }
+    toArray() {
+        return this.mas;
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    sort(indices) {
+        indices.sort(function compareNumeric(a, b) {
+            return a - b;
+        });
+        var mas2 = [];
+        for (var i = 0; i < indices.length; i++) {
+            mas2.push(this.mas[indices[i]])
+        }
+        mas2.sort(this.compareFunction);
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+        for (var i = 0; i < indices.length; i++) {
+            this.mas[indices[i]] = mas2[i];
+        }
+    }
+
+    setComparator(compareFunction) {
+        this.compareFunction = compareFunction;
+    }
 }
 
 module.exports = Sorter;
